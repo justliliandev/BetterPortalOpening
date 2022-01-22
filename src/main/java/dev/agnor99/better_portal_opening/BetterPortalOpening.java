5a43f9f3f0a6c4078605ec2711afcc9dcd8f40a8
agnor99 <kreader100@gmail.com>
2022-01-22 14:46:03 +0100

    1.17.1

--- src/main/java/dev/agnor99/better_portal_opening/BetterPortalOpening.java
+++ src/main/java/dev/agnor99/better_portal_opening/BetterPortalOpening.java
@@ -1,15 +1,13 @@
-package com.agnor99.better_portal_opening;
+package dev.agnor99.better_portal_opening;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BetterPortalOpening.MOD_ID)
 public class BetterPortalOpening {
     public static final String MOD_ID = "better_portal_opening";
 
     public BetterPortalOpening() {
-        MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(PortalOpener.class);
     }
 }
---
+++ src/main/java/dev/agnor99/better_portal_opening/PortalOpener.java
@@ -0,0 +1,74 @@
+package dev.agnor99.better_portal_opening;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.level.portal.PortalShape;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Optional;
+
+@Mod.EventBusSubscriber
+public class PortalOpener {
+
+    @SubscribeEvent
+    public static void onArrowImpact(ProjectileImpactEvent event) {
+
+        if (event.getProjectile() instanceof AbstractArrow arrow
+            && arrow.isOnFire()
+            && event.getRayTraceResult() instanceof BlockHitResult hitResult
+            && createPortal(arrow.level, hitResult.getBlockPos().relative(hitResult.getDirection()))) {
+
+            arrow.remove(Entity.RemovalReason.DISCARDED);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onSwordSwing(PlayerInteractEvent.LeftClickBlock event) {
+        if (validWorld(event.getWorld())) {
+            ItemStack stack = event.getItemStack();
+            if (!stack.isEmpty()
+                && stack.getItem() != Items.ENCHANTED_BOOK
+                && stack.isEnchanted()
+                && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FIRE_ASPECT, stack) != 0
+                && createPortal(event.getWorld(), event.getPos().relative(event.getFace()))
+                && stack.isDamageableItem()) {
+
+                stack.hurtAndBreak(1,
+                            event.getPlayer(),
+                            player -> player.broadcastBreakEvent(event.getHand())
+                    );
+
+            }
+        }
+    }
+
+
+    private static boolean createPortal(Level world, BlockPos pos) {
+        if (validWorld(world)) {
+            Optional<PortalShape> optional = PortalShape.findEmptyPortalShape(world, pos, Direction.Axis.X);
+            optional = ForgeEventFactory.onTrySpawnPortal(world, pos, optional);
+            if (optional.isPresent()) {
+                optional.get().createPortalBlocks();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean validWorld(Level world){
+        return !world.isClientSide() && world.dimension() == Level.OVERWORLD || world.dimension() == Level.NETHER;
+    }
+}
